d981492d288996c6518d566042e48da69f627a95
Cristian Necochea <cristiannecochea@Cristians-iMac.home>
2019-05-17 01:49:40 -0400

    Structure and props for Layout component

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -3,17 +3,22 @@ import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
 const StyledLayout = styled.div`
+  background-color: ${props => props.theme.bg};
+  color: ${props => props.theme.fg};
   display: grid;
   grid-gap: 1rem;
+  min-height: 100vh;
 
   > * {
-    outline: 1px dotted red;
+    outline: 1px dotted ${props => props.theme.highlight};
   }
 `;
 
 const StyledAside = styled.aside``;
 
-const StyledTopNavigation = styled.div``;
+const StyledTopNavigation = styled.div`
+  grid-area: top;
+`;
 
 const StyledNotification = styled.div``;
 
@@ -21,23 +26,41 @@ const StyledSideNavigation = styled.div``;
 
 const StyledFooter = styled.footer``;
 
-const Layout = ({ Aside, children, Footer, SideNavigation, TopNavigation }) => (
-  <StyledLayout data-desc="A reusable layout component">
-    <>{TopNavigation}</>
-    <>{Notification}</>
-    <>{SideNavigation}</>
+const Layout = ({
+  Aside,
+  asideContent,
+  children,
+  Footer,
+  footerContent,
+  Notification,
+  notificationContent,
+  SideNavigation,
+  sideContent,
+  TopNavigation,
+  topContent,
+  ...props
+}) => (
+  <StyledLayout {...props}>
+    {topContent ? <TopNavigation>{topContent}</TopNavigation> : null}
+    {notificationContent ? <Notification>{notificationContent}</Notification>: null}
+    {sideContent ? <SideNavigation>{sideContent}</SideNavigation> : null}
     <main>{children}</main>
-    <>{Aside}</>
+    {asideContent ? <Aside>{asideContent}</Aside> : null}
   </StyledLayout>
 );
 
 Layout.propTypes = {
   Aside: PropTypes.func,
+  asideContent: PropTypes.node,
   children: PropTypes.node,
   Notification: PropTypes.func,
+  notificationContent: PropTypes.node,
   SideNavigation: PropTypes.func,
+  sideContent: PropTypes.node,
   TopNavigation: PropTypes.func,
+  topContent: PropTypes.node,
   Footer: PropTypes.func,
+  footerContent: PropTypes.node,
 };
 
 Layout.defaultProps = {
