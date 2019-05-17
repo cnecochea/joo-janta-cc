5dc14cee88cb349481caf510b60449ae38525fbe
Cristian Necochea <cristiannecochea@Cristians-iMac.home>
2019-05-17 02:58:14 -0400

    Configure storybook to use global styles / themes

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,11 +1,28 @@
+import React from 'react';
 import { addDecorator, configure, setAddon } from '@storybook/react';
 import JSXAddon from 'storybook-addon-jsx';
 import { withThemes } from 'storybook-styled-components';
+import { themes } from '../src/shared/Themes';
+import { GlobalStyle } from '../src/shared/Styles';
 
 setAddon(JSXAddon);
 
-// const themes = ['light', 'dark'];
-// addDecorator(withThemesProvider(themes));
+const THEME = {
+  'Home theme': themes.home,
+  'Away theme': themes.away,
+  'Winter Classic theme': themes.winter,
+}
+
+const withGlobal = (s) => (
+  <>
+    <GlobalStyle />
+    {s()}
+  </>
+);
+
+addDecorator(withGlobal);
+
+addDecorator(withThemes(THEME));
 
 function loadStories() {
   require('../src/stories');
